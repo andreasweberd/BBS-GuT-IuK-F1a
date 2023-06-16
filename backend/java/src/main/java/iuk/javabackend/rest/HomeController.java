082d3121278d6cba198441132a7ecb7c9a26729f
Gwanny <itsame@gwanny.de>
2023-06-16 11:51:59 +0200

    HomeController.java Beispiel hinzugefügt & ungebrauchte Imports entfernt

--- backend/java/src/main/java/iuk/javabackend/rest/HomeController.java
+++ backend/java/src/main/java/iuk/javabackend/rest/HomeController.java
@@ -1,15 +1,12 @@
 package iuk.javabackend.rest;
 
-import jdk.jfr.ContentType;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/api/test")
@@ -18,6 +15,14 @@ public class HomeController {
     @GetMapping
     public List<String> test() {
         return List.of("Hello World", "Mothafuckaaaa!");
+    }
+
+    @GetMapping("/abc")
+    public Map<String, Integer> test2() {
+        Map<String, Integer> test = new HashMap<>();
+        test.put("Test", 1);
+        test.put("Bsp", 2);
+        return test;
     }
 
 }
