b3cf443492aa2401e429878fbbd0995d2270f53e
Philipp Schuster <philipp.schuster@dedalus.com>
2023-06-16 11:02:48 +0200

    Structure changed

--- BBSPizzaFrontend/src/app/app.module.ts
+++ BBSPizzaFrontend/src/app/app.module.ts
@@ -5,18 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from 'src/home/home.component';
-import { NotfoundpageComponent } from 'src/notfoundpage/notfoundpage.component';
+import { HomeComponent } from 'src/app/pages/home/home.component';
+import { NotfoundpageComponent } from 'src/app/pages/notfoundpage/notfoundpage.component';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import { OffersComponent } from './pages/offers/offers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-      NotfoundpageComponent
+      NotfoundpageComponent,
+      OffersComponent
    ],
   imports: [
     BrowserModule,
