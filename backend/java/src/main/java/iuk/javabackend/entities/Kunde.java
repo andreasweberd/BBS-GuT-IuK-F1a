645af91fc1a5817e8640caa06ecb68f4880d716a
MoAlhabash <mouhamad.alhabash@gmail.com>
2023-06-16 11:55:44 +0200

    Add the bestellung and the kunde enteties.

---
+++ backend/java/src/main/java/iuk/javabackend/entities/Kunde.java
@@ -0,0 +1,26 @@
+package iuk.javabackend.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+public class Kunde {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String vorname;
+    private String nachname;
+    private Date geburtsdatum;
+    private int telefonnummer;
+    private int zahlungsart;
+
+
+
+}
