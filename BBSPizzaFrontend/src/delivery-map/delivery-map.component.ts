43e983f4714914896e7bca4d19a1b98b509eff6c
Eric Scherlich <eric.scherlich@gmail.com>
2023-06-30 10:54:19 +0200

    map with inital marker of doeminos

--- BBSPizzaFrontend/src/delivery-map/delivery-map.component.ts
+++ BBSPizzaFrontend/src/delivery-map/delivery-map.component.ts
@@ -1,26 +1,66 @@
 import { Component } from '@angular/core';
 import * as Leaflet from 'leaflet';
 
+Leaflet.Icon.Default.imagePath = 'assets/';
 @Component({
   selector: 'app-delivery-map',
   templateUrl: './delivery-map.component.html',
   styleUrls: ['./delivery-map.component.scss']
 })
 export class DeliveryMapComponent {
-  options: Leaflet.MapOptions = {
-    layers: getLayers(),
-    zoom: 12,
-    center: new Leaflet.LatLng(43.530147, 16.488932)
-  };
-}
+  map!: Leaflet.Map;
+  markers: Leaflet.Marker[] = [];
+  options = {
+    layers: [
+      Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      })
+    ],
+    zoom: 14,
+    center: { lat: 49.75843, lng: 6.64409 }
+  }
 
-export const getLayers = (): Leaflet.Layer[] => {
-  return [
-    new Leaflet.TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; OpenStreetMap contributors'
-    } as Leaflet.TileLayerOptions),
-  ] as Leaflet.Layer[];
-};
+  initMarkers() {
+    const initialMarkers = [
+      {
+        position: { lat: 49.7593244, lng: 6.6354235 },
+        draggable: false,
+        description: "Döminos"
+      }
+    ];
+    for (let index = 0; index < initialMarkers.length; index++) {
+      const data = initialMarkers[index];
+      const marker = this.generateMarker(data, index);
+      marker.addTo(this.map).bindPopup(`<b>${data.description}</b>`);
+      this.map.panTo(data.position);
+      this.markers.push(marker)
+    }
+  }
+  
+
+  generateMarker(data: any, index: number) {
+    return Leaflet.marker(data.position, { draggable: data.draggable })
+      .on('click', (event) => this.markerClicked(event, index))
+      .on('dragend', (event) => this.markerDragEnd(event, index));
+  }
+
+  onMapReady($event: Leaflet.Map) {
+    this.map = $event;
+    this.initMarkers();
+  }
+
+  mapClicked($event: any) {
+    console.log($event.latlng.lat, $event.latlng.lng);
+  }
+
+  markerClicked($event: any, index: number) {
+    console.log($event.latlng.lat, $event.latlng.lng);
+  }
+
+  markerDragEnd($event: any, index: number) {
+    console.log($event.target.getLatLng());
+  } 
+}
 
 
 
