843c6e503f9d12b0dfc3c80cf400188852e1cee2
Torben Weber <TorbenWeber06@gmx.de>
2023-06-30 11:08:29 +0200

    Verbindung zwischen Pizza und Zutaten gefixed.
    KundenRepository & KundenService angelegt.

--- backend/java/src/main/java/iuk/javabackend/JavaApplication.java
+++ backend/java/src/main/java/iuk/javabackend/JavaApplication.java
@@ -1,20 +1,14 @@
 package iuk.javabackend;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication()
 @Configuration
 public class JavaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JavaApplication.class, args);
     }
-
-
 }
