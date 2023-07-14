c28374a1f2a4cee1f47d266c8726f16c7e7daedd
Nirfu <48183175+Nirfu@users.noreply.github.com>
2023-07-14 11:00:15 +0200

    Edited and tested Setup script

--- install_mysql.sh
+++ install_mysql.sh
@@ -4,8 +4,9 @@ rpm -Uvh mysql80-community-release-el9-1.noarch.rpm
 dnf install mysql-community-server
 systemctl start mysqld
 echo "\nbind-address=0.0.0.0" >> /etc/my.cnf
+read -p "Enter new root password: " newRootPass
+read -p "Enter team_db password: " teamdbPass
+read -p "Enter team_cs password: " teamcsPass
+read -p "Enter team_java password: " teamjavaPass
 grep 'temporary password' /var/log/mysqld.log
-read -p "Enter temporary root password: " password
-newRootPass = '4bX!k8gM'
-mysql -uroot -p$password -Bse "ALTER USER 'root'@'localhost' IDENTIFIED BY $newrootPass;CREATE DATABASE pizzeria;CREATE USER 'team_db'@'%' IDENTIFIED BY '9n3c&2D2';CREATE DATABASE team_cs;CREATE USER 'team_cs'@'%' IDENTIFIED BY '7b3Tr%\!a';CREATE DATABASE team_java;CREATE USER 'team_java'@'%' IDENTIFIED BY '5b\$2wQNj';GRANT ALL PRIVILEGES ON pizzeria.* TO 'team_db'@'%';GRANT ALL PRIVILEGES ON pizzeria.* TO 'team_java'@'%';GRANT ALL PRIVILEGES ON team_cs.* TO 'team_cs'@'%';GRANT ALL PRIVILEGES ON team_java.* TO 'team_java'@'%';"
-echo "\n[client]\nuser=root\npassword=$newRootPass" >> /etc/my.cnf
+mysql -uroot -p -B --execute="ALTER USER 'root'@'localhost' IDENTIFIED BY '$newRootPass';CREATE DATABASE pizzeria;CREATE USER 'team_db'@'%' IDENTIFIED BY '$teamdbPass';CREATE DATABASE team_cs;CREATE USER 'team_cs'@'%' IDENTIFIED BY '$teamcsPass';CREATE DATABASE team_java;CREATE USER 'team_java'@'%' IDENTIFIED BY '$teamjavaPass';GRANT ALL PRIVILEGES ON pizzeria.* TO 'team_db'@'%';GRANT ALL PRIVILEGES ON pizzeria.* TO 'team_java'@'%';GRANT ALL PRIVILEGES ON team_cs.* TO 'team_cs'@'%';GRANT ALL PRIVILEGES ON team_java.* TO 'team_java'@'%';"
